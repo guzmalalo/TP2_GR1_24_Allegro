a5fe13c8b3ec5c269efdb3ee68a3f9b69ef430fa
Eduardo Guzman <eduardo.guzman@innovamics.com>
2024-03-22 09:12:22 +0100

    correction TP2

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <allegro5/allegro_primitives.h>
 
 #include "rectangle.h"
+enum{LEFT, UP, DOWN , RIGHT};
 
 int main() {
     // Declarations
@@ -13,8 +14,7 @@ int main() {
     ALLEGRO_EVENT event = {0};
     RECTANGLE p = {0, 0, 50, 30, 10, 20};
     bool fini = false;
-    bool flag_left = false;
-    bool flags [4];
+    bool flags[4]= {false};
 
     // Initialisation
     assert(al_init());
@@ -47,11 +47,36 @@ int main() {
                 fini = true;
                 break;
             case ALLEGRO_EVENT_TIMER:
-                if (flag_left) {
+                if (flags[LEFT]) {
                     if (p.x > 0) {
                         p.x = p.x - p.dx;
+                    }else{
+                        p.x = 0;
                     }
                 }
+                if(flags[RIGHT]){
+                    if(p.x < (WDISPLAY-p.w)) {
+                        p.x = p.x + p.dx;
+                    }else{
+                        p.x = WDISPLAY - p.w;
+                    }
+                }
+                if(flags[UP]) {
+                    if (p.y > 0) {
+                        p.y = p.y - p.dy;
+                    }else{
+                        p.y = 0;
+                    }
+                }
+                if(flags[DOWN]) {
+                    if((p.y+p.h) < HDISPLAY){
+                        p.y = p.y + p.dy;
+                    }else{
+                        p.y = HDISPLAY - p.h;
+                    }
+                }
+
+
                 al_clear_to_color(al_map_rgb(0,0,0));
                 al_draw_filled_rectangle(p.x, p.y, p.x + p.w, p.y +p.h,red);
                 al_flip_display();
@@ -59,33 +84,36 @@ int main() {
             case ALLEGRO_EVENT_KEY_DOWN:
                 switch (event.keyboard.keycode) {
                     case ALLEGRO_KEY_LEFT:
-                        flag_left = true;
+                        flags[LEFT] = true;
                         break;
                     case ALLEGRO_KEY_RIGHT:
-                        if(p.x < (WDISPLAY-p.w)) {
-                            p.x = p.x + p.dx;
-                        }
+                        flags[RIGHT] = true;
                         break;
                     case ALLEGRO_KEY_UP:
-                        if(p.y>0){
-                            p.y = p.y - p.dy;
-                        }
+                        flags[UP]= true;
                         break;
                     case ALLEGRO_KEY_DOWN:
-                        if((p.y+p.h) < HDISPLAY){
-                            p.y = p.y + p.dy;
-                        }
+                        flags[DOWN] = true;
                         break;
-
                 }
                 // gerer le déplacement
                 break;
-            case ALLEGRO_KEY_UP:
+            case ALLEGRO_EVENT_KEY_UP:
                 switch (event.keyboard.keycode) {
                     case ALLEGRO_KEY_LEFT:
-                        flag_left = false;
+                        flags[LEFT] = false;
+                        break;
+                    case ALLEGRO_KEY_RIGHT:
+                        flags[RIGHT] = false;
+                        break;
+                    case ALLEGRO_KEY_UP:
+                        flags[UP]= false;
+                        break;
+                    case ALLEGRO_KEY_DOWN:
+                        flags[DOWN] = false;
                         break;
                 }
+                break;
         }
 
 
